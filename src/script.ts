0a5e24fbfea1b4824ecc362e6c7070aec91e7f4e
Traveler59 <dmitry_alekseevich@mail.ru>
2016-09-30 13:46:53 +0300

    Исправлены недоработки кода fix #3

--- src/script.ts
+++ src/script.ts
@@ -1,46 +1,52 @@
-class ReservationsViewModel {
-  num: number;
-  rightAnswers: KnockoutObservable<number>;
-  availableMeals: Array<any>;
-  currentItem: KnockoutObservable<any>;
-  showResults: KnockoutObservable<Boolean>;
-  testProgress: KnockoutObservable<String>;
+interface Meal{
+    mealName: string;
+    img: string;
+    wine: string;
+}
+
+class TestViewModel {
+    currentMealNumber: number;
+    rightAnswers: KnockoutObservable<number>;
+    availableMeals: Array<Meal>;
+    currentItem: KnockoutObservable<Meal>;
+    showResults: KnockoutObservable<boolean>;
+    testProgress: KnockoutObservable<string>;
 
     constructor() {
-      var self = this;
-      this.num = 0;
-      this.rightAnswers = ko.observable(0);
-      this.showResults = ko.observable(false);
-      this.testProgress = ko.observable("0%");
+        this.currentMealNumber = 0;
+        this.rightAnswers = ko.observable(0);
+        this.showResults = ko.observable(false);
+        this.testProgress = ko.observable("0%");
 
-      this.availableMeals = [
-          { mealName: "Сыр", img: "images/Cheese.jpg", wine: "Red" },
-          { mealName: "Красное Мясо", img: "images/RedMeat.jpg", wine: "Red" },
-          { mealName: "Рыба", img: "images/Fish.jpg", wine: "White" },
-          { mealName: "Шоколад", img: "images/Chocolate.jpg", wine: "Sweet" },
-          { mealName: "Икра", img: "images/Caviar.jpg", wine: "White" },
-          { mealName: "Мороженное", img: "images/Ice Cream.png", wine: "Sweet" },
-          { mealName: "Апельсины", img: "images/Orange.jpg", wine: "Red" }
-      ];
+        this.availableMeals = [
+            { mealName: "Сыр", img: "images/Cheese.jpg", wine: "Red" },
+            { mealName: "Красное Мясо", img: "images/RedMeat.jpg", wine: "Red" },
+            { mealName: "Рыба", img: "images/Fish.jpg", wine: "White" },
+            { mealName: "Шоколад", img: "images/Chocolate.jpg", wine: "Sweet" },
+            { mealName: "Икра", img: "images/Caviar.jpg", wine: "White" },
+            { mealName: "Мороженное", img: "images/Ice Cream.png", wine: "Sweet" },
+            { mealName: "Апельсины", img: "images/Orange.jpg", wine: "Red" }
+        ];
 
-      this.currentItem = ko.observable(this.availableMeals[self.num]);
-  }
+        this.currentItem = ko.observable(this.availableMeals[this.currentMealNumber]);
+    }
 
-    check(data: any, event: any): void {
-      if(this.num != this.availableMeals.length) {
-        this.updateProgressBar();
-        if(this.currentItem().wine == event.currentTarget.getAttribute("data-wine"))
-          this.rightAnswers(this.rightAnswers() + 1);
-        if(++this.num < this.availableMeals.length)
-         this.currentItem(this.availableMeals[this.num]);
-        else this.showResults(true);
-      }
+    check(answer: string): void {
+        if (this.currentMealNumber != this.availableMeals.length) {
+            this.updateProgressBar();
+            if (this.currentItem().wine == answer)
+                this.rightAnswers(this.rightAnswers() + 1);
+
+            if (++this.currentMealNumber < this.availableMeals.length)
+                this.currentItem(this.availableMeals[this.currentMealNumber]);
+            else this.showResults(true);
+        }
     }
 
     updateProgressBar(): void {
-      let value: number = (this.num + 1) * 100 / this.availableMeals.length;
-      this.testProgress(value+'%');
+        const value = (this.currentMealNumber + 1) * 100 / this.availableMeals.length;
+        this.testProgress(value+'%');
     }
 }
 
-ko.applyBindings(new ReservationsViewModel());
+ko.applyBindings(new TestViewModel());
